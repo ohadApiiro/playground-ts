b891493b244263c42c10fe85087b9f15932f1a2c
ohadApiiro <ohad@apiiro.com>
2022-07-13 17:16:57 +0300

    first

---
+++ app.ts
@@ -0,0 +1,33 @@
+//import * as Proto from './protocol';
+
+let message: string = 'Hello, World!';
+console.log(message);
+
+
+class User {
+    userName: string;
+    'userId': string;
+    'creditCard': string;
+    'address': string;
+    'phone': string;
+    'user-data-dir': string;
+    'rename': string;
+    'getEditsForFileRename': string
+}
+
+class Person {
+    'name': string;
+    id: bigint;
+}
+
+const usr: User = {
+    "user-data-dir": 'ba',
+    address: 'faf',
+    userId: 'fdf',
+    userName: 'afdfd',
+    phone: '1233',
+    creditCard: '36454',
+    'rename': 'nbnn',
+    'getEditsForFileRename': 'dfdggg'
+}
+console.log(usr)
