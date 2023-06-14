9434b7e772d9c3c5c61a2cefb46041f4fd530072
ohadApiiro <ohad@apiiro.com>
2023-06-14 15:45:08 +0300

    first

---
+++ app.ts
@@ -0,0 +1,46 @@
+//import * as Proto from './protocol';
+
+// let message: string = 'Hello, World!';
+// console.log(message);
+
+
+function add(x, y) {
+    return x + y;
+}
+
+add(1,2);
+
+// class User {
+//     'userName': string;
+//     'userId': string;
+//     'creditCard': string;
+//     'address': string;
+//     'phone': string;
+//     'user-data-dir': string;
+//     'rename': string;
+//     'getEditsForFileRename': string
+// }
+//
+// class Person {
+//     'name': string;
+//     id: number;
+// }
+//
+// const person: Person  = {
+//     id: 1,
+//     name: "a"
+// }
+//
+// //console.log(person);
+//
+// const usr: User = {
+//     "user-data-dir": 'ba',
+//     address: 'faf',
+//     userId: 'fdf',
+//     userName: 'afdfd',
+//     phone: '1233',
+//     creditCard: '36454',
+//     'rename': 'nbnn',
+//     'getEditsForFileRename': 'dfdggg'
+// }
+//console.log(usr)
